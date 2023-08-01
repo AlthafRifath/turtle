0a7a0021e1930c481e5d76513f802aa3a994338d
Althaf <althafrifath@gmail.com>
2023-08-02 01:49:19 +0530

    JSON setup Successfull

--- javaScript/Tickets.js
+++ javaScript/Tickets.js
@@ -1,10 +1,40 @@
 console.info("Ticket Page Loaded");
 
 document.addEventListener("alpine:init", () => {
-    Alpine.data('tickets', {
-        gotoDetailsPage() {
-            localStorage.setItem("tickets", JSON.stringify(this.ticket));
-            window.location.href = "Details.html";
-        }
-    });
+    
+    fetch('JSON/Tickets.json')
+        .then(response => response.json())
+        .then(jsonData => {
+            console.log('Data fetched:', jsonData);
+            initializeApp(jsonData);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+    
+    function initializeApp(jsonData) {
+        Alpine.data('tickets', {
+            selectedTimeSlot: '',
+            selectedDate: '',
+            slAdultTickets: 0,
+            slChildTickets: 0,
+            foreignerAdultTickets: 0,
+            foreignerChildTickets: 0,
+            durations: jsonData.durations,
+
+            gotoDetailsPage() {
+                const inputData = {
+                    selectedTimeSlot: this.selectedTimeSlot,
+                    selectedDate: this.selectedDate,
+                    slAdultTickets: this.slAdultTickets,
+                    slChildTickets: this.slChildTickets,
+                    foreignerAdultTickets: this.foreignerAdultTickets,
+                    foreignerChildTickets: this.foreignerChildTickets,
+                };
+                localStorage.setItem('ticketsData', JSON.stringify(inputData));
+                window.location.href = "Details.html";
+            }
+        });
+        console.log("Tickets JSON Data", JSON.stringify(jsonData));
+    }
 });
