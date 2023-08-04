ce61d9410f2a08d3cc51523aea557981efe93566
Althaf <althafrifath@gmail.com>
2023-08-04 21:56:06 +0530

    Final Push for Today 8/4/2023

--- javaScript/Tickets.js
+++ javaScript/Tickets.js
@@ -24,19 +24,19 @@ fetch('JSON/Tickets.json')
   .then(jsonData => {
     console.log('Data fetched:', jsonData);
     this.duration = jsonData.duration;
-    document.addEventListener('DOMContentLoaded', function () {
-      const timeSlotSelect = document.getElementById('timeSlot');
-      console.log('Select element:', timeSlotSelect); // Check if the select element is found
 
-      jsonData.durations.forEach((duration => {
-        const option = document.createElement('option');
-        option.value = "${duration.start}-${duration.end}";
-        option.textContent = `${duration.start} - ${duration.end} ${duration.duration === "Peak" ? "(Peak)" : ""}`;
-        timeSlotSelect.appendChild(option);
-        console.log('Appended option:', option); // Check if the option is being created and appended
+    const timeSlotSelect = document.getElementById('timeSlot');
+    console.log('Select element:', timeSlotSelect); // Check if the select element is found
 
-      }))
+    jsonData.durations.forEach((duration) => {
+      const option = document.createElement('option');
+      const optionValue = `${duration.start}-${duration.end}`; // Create the value for the option
+      option.value = optionValue; // Set the value for the option
+      option.textContent = `${duration.start} - ${duration.end} ${duration.duration === "Peak" ? "(Peak)" : ""}`;
+      timeSlotSelect.appendChild(option);
+      console.log('Appended option:', option); // Check if the option is being created and appended
     });
+
   })
   .catch(error => {
     console.error('Error fetching data:', error);
