c9f792e99a28e8802fed8f375d5059e3410bb614
Althaf <althafrifath@gmail.com>
2023-08-06 12:58:03 +0530

    Tickets Page Done

--- javaScript/Tickets.js
+++ javaScript/Tickets.js
@@ -1,51 +1,51 @@
-console.info("Ticket Page Loaded");
+console.log("Tickets Page Loaded");
 
-// Alpine JS
 document.addEventListener("alpine:init", () => {
-  Alpine.data('ticketsModel', {
-    
-    selectedTimeSlot(index) {
-      if (this.selectedTimeSlot.includes(index)) {
-        this.selectedTimeSlot = this.selectedTimeSlot.filter((i) => i !== index);
-      } else {
-        let lastElement = this.selectedTimeSlot[this.selectedTimeSlot.length - 1];
-        if (!this.selectedTimeSlot.length || (index - lastElement === 1)) {
-          this.selectedTimeSlot.push(index);
-        } else {
-          alert('Please select the time slots in order');
-        }
-      }
-    },
+    Alpine.data('ticketsData', () => ({
+        selectedTimeSlot: "",
+        durations: [],
+        selectedDate: null,
+        slAdultTickets: 0,
+        slChildTickets: 0,
+        foreignerAdultTickets: 0,
+        foreignerChildTickets: 0,
+        slAdultCharges: 0, 
+        slChildCharges: 0,
+        foreignerAdultCharges: 0,
+        foreignerChildCharges: 0,
+        totalCharges: 0,
 
-    init() {
-      console.log("Alpine Store Loaded");
+        fetchData() {
+            console.log("Tickets JSON Data Loaded");
+            fetch('JSON/Tickets.json')
+                .then(response => response.json())
+                .then(jsonData => {
+                    console.log('Data Fetched:', jsonData);
+                    this.durations = jsonData.durations; // Assign durations from JSON
+                    this.calculateCharges(); // Call calculateCharges after data is fetched
+                })
+                .catch(error => console.error('Error Fetching Data', error));
+        },
 
-    }
-  });
+        formatDuration(duration) {
+            return `${duration.start} - ${duration.end} ${duration.duration === "Peak" ? "(Peak)" : ""}`;
+        },
+
+        getSelectedDuration() {
+            return this.durations.find(duration => this.selectedTimeSlot.includes(duration.start));
+        },
+
+        calculateCharges() {
+            const selectedDuration = this.getSelectedDuration();
+            if (!selectedDuration) return; // Handle case when selectedDuration is not found
+            const charges = selectedDuration.charges;
+
+            this.slAdultCharges = charges.slAdult * this.slAdultTickets;
+            this.slChildCharges = charges.slChild * this.slChildTickets;
+            this.foreignerAdultCharges = charges.foreignerAdult * this.foreignerAdultTickets;
+            this.foreignerChildCharges = charges.foreignerChild * this.foreignerChildTickets;
+
+            this.totalCharges = this.slAdultCharges + this.slChildCharges + this.foreignerAdultCharges + this.foreignerChildCharges;
+        },
+    }));
 });
-
-
-// Vanilla JS
-console.log('data store loaded');
-fetch('JSON/Tickets.json')
-  .then(response => response.json())
-  .then(jsonData => {
-    console.log('Data fetched:', jsonData);
-    this.duration = jsonData.duration;
-
-    const timeSlotSelect = document.getElementById('timeSlot');
-    console.log('Select element:', timeSlotSelect); // Check if the select element is found
-
-    jsonData.durations.forEach((duration) => {
-      const option = document.createElement('option');
-      const optionValue = `${duration.start}-${duration.end}`; // Create the value for the option
-      option.value = optionValue; // Set the value for the option
-      option.textContent = `${duration.start} - ${duration.end} ${duration.duration === "Peak" ? "(Peak)" : ""}`;
-      timeSlotSelect.appendChild(option);
-      console.log('Appended option:', option); // Check if the option is being created and appended
-    });
-
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
