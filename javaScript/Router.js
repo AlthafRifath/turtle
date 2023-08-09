e74dc022c6f5e4916d1928e2348332b672961ecf
Althaf <althafrifath@gmail.com>
2023-08-09 16:20:25 +0530

    Router Buttoning Fixed

--- javaScript/Router.js
+++ javaScript/Router.js
@@ -3,7 +3,7 @@ console.info("Router Page Loaded");
 document.addEventListener("alpine:init", () => {
     Alpine.store('router', {
         currentPage: '',
-
+        
         goto(page) {
             this.currentPage = page;
             window.location.href = `${page}.html`;
